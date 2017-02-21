c6560fb944847f8d3c861616c155ac21f76494a7
Dinesh Balasubramanian <dineshudt17@gmail.com>
2017-02-21 14:50:23 +0530

    [Dinesh/Harini] Using collectionview for showing items

--- Feedback/FeedbackView.swift
+++ Feedback/FeedbackView.swift
@@ -1,15 +1,18 @@
 import UIKit
 
-class FeedbackView: UIViewController, FeedbackViewProtocol {
+class FeedbackView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, FeedbackViewProtocol{
     var presenter: FeedbackPresenterProtocol?
     
     @IBOutlet weak var titleText: UILabel!
     
     @IBOutlet weak var itemsScrollView: UIScrollView!
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let myImages = ["splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png"]
+
     func showFeedback(feedback: FeedbackModel){
         titleText.text = feedback.name
-        initializeScrollBar()
     }
 
     override func viewDidLoad() {
@@ -21,40 +24,20 @@ class FeedbackView: UIViewController, FeedbackViewProtocol {
         super.didReceiveMemoryWarning()
     }
     
-    
-    func initializeScrollBar() {
-        
-        let myImages = ["splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png","splash.png"]
-        
-        let imageWidth:CGFloat = 66
-        let imageHeight:CGFloat = 66
-        let spacer: CGFloat = 10
-        var xPosition:CGFloat = 0
-        var scrollViewSize:CGFloat=0
-        var index = 0
-        
-        for image in myImages {
-            
-            let myImage:UIImage = UIImage(named: image)!
-            let uiButton = UIButton()
-            uiButton.setImage(myImage, for: UIControlState.normal)
-            
-            uiButton.frame.size.width = imageWidth
-            uiButton.frame.size.height = imageHeight
-            uiButton.frame.origin.x = xPosition
-            uiButton.frame.origin.y = 10
-//            uiButton.restorationIdentifier = String(index)
-//            uiButton.setTitle(String(index), for: UIControlState.normal)
-            index+=1
-            
-            itemsScrollView.addSubview(uiButton)
-            xPosition += imageWidth + spacer
-            scrollViewSize += (imageWidth)
-            
-        }
-        
-        itemsScrollView.contentSize = CGSize(width: scrollViewSize + 200, height: imageHeight)
-        
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return myImages.count
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ItemCellView = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ItemCellView
+        cell.image.image = UIImage(named: myImages[indexPath.row])
+        cell.name.text = "HI"
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Cell \(indexPath.row) selected")
+    }
+    
 }
 
---
+++ Feedback/ItemCellView.swift
@@ -0,0 +1,15 @@
+//
+//  itemCell.swift
+//  Feedback
+//
+//  Created by DINESH BALASUBARMANIAM on 21/02/17.
+//  Copyright © 2017 Thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class ItemCellView: UICollectionViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var image: UIImageView!
+}

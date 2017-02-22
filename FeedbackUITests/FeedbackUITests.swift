5cf27ea55b01ec32594029eb5f43f656097f7144
Arun Sundaralingam <arunsundaralingam@IN-CHNI-Arun-Sundaralingam.local>
2017-02-22 19:48:10 +0530

    [Arun/Priya] fixing UI test

--- FeedbackUITests/FeedbackUITests.swift
+++ FeedbackUITests/FeedbackUITests.swift
@@ -8,7 +8,7 @@ import Cuckoo
 extension XCUIApplication {
     func launchTestsWithEnvironmentVariables() {
         launchEnvironment = [
-            "FEEDBACK_BACKEND_URL" : "mywebservice.com"
+            "FEEDBACK_BACKEND_URL" : "http://12.12.12.12:8000"
         ]
         self.launch()
     }
@@ -29,18 +29,18 @@ class FeedbackUITests: XCTestCase {
         super.tearDown()
     }
     
-//    func testExample() {
-//        let stubbedInterviews = "Interview 1"
-//        stub(condition: isHost("mywebservice.com")) { _ in
-//            let interview = [stubbedInterviews]
-//            let stubData: Data = NSKeyedArchiver.archivedData(withRootObject: interview) as Data
-//            return OHHTTPStubsResponse(data: stubData, statusCode: 200, headers: nil)
-//        }
-//        let cells = app?.collectionViews.cells;
-//        XCTAssert(app?.staticTexts.element(matching: .any, identifier: "Feedback123").label != nil)
-//        let cellOne = cells?.element(boundBy: 0)
-//        XCTAssertEqual(cells?.count,3)
-//        XCTAssert(cellOne?.tap() != nil)
-//        XCTAssert(app?.staticTexts.element(matching: .any, identifier: "tapped").label != nil)
-//    }
+    func testExample() {
+        let stubbedInterviews = "Interview 1"
+        stub(condition: isHost("stubwebser")) { _ in
+            let interview = [stubbedInterviews]
+            let stubData: Data = NSKeyedArchiver.archivedData(withRootObject: interview) as Data
+            return OHHTTPStubsResponse(data: stubData, statusCode: 200, headers: nil)
+        }
+        let cells = app?.collectionViews.cells;
+        XCTAssert(app?.staticTexts.element(matching: .any, identifier: "Feedback123").label != nil)
+        let cellOne = cells?.element(boundBy: 0)
+        XCTAssertEqual(cells?.count,3)
+        XCTAssert(cellOne?.tap() != nil)
+        XCTAssert(app?.staticTexts.element(matching: .any, identifier: "tapped").label != nil)
+    }
 }
